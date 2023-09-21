34db98dae1f1af59b64a2545817d0615a9aa564c
vfmenna <145074986+vfmenna@users.noreply.github.com>
2023-09-21 16:32:08 -0700

    Add files via upload

--- Sept_21/sinewave.m
+++ Sept_21/sinewave.m
@@ -1,10 +1,13 @@
+%% Tutorial 4.2.1 for Loop (sinewave)
+
 % Clear out the workspace
 clear
 
 % Generate the vectors to be plotted
-for i = 1:11
-    x(i) = (1 - i)*(2*pi/10);
+for i = 1:1001
+    x(i) = (1 - i)*(2*pi/1000);
     y(i) = sin(x(i));
 end
 
-plot(x,y)
+plot(x,y,'LineWidth',2)
+grid on
