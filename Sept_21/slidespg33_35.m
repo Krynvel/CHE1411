4cb18030c0a55ab1c6da84449ce9a821880561a9
vfmenna <145074986+vfmenna@users.noreply.github.com>
2023-09-21 17:27:27 -0700

    Add files via upload

---
+++ Sept_21/slidespg33_35.m
@@ -0,0 +1,34 @@
+%% Chapter 4 - Part 1 Flow Chart and Loops Examples
+%% Slide 33
+
+m = 0;
+A = 20;
+while A <= 50
+    A = A + 5;
+    m = m + 1;
+end
+A
+m
+
+%% Slide 34
+
+m = 0;
+A = 100;
+while A > 15
+    A = A/2;
+    m = m +1;
+end
+A 
+m
+
+%% Slide 35
+
+m = 0;
+A = 10;
+while A > 0
+    A = sqrt(A);
+    m = m + 1;
+end
+A 
+m
+% this will print an infinite loop. A screenshot will be included.
