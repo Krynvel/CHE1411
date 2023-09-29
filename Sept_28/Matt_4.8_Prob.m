db46a27b432bca8e0887e8049568a368b6ed61cd
Matthew Sianarta <71192423+Krynvel@users.noreply.github.com>
2023-09-28 17:53:22 -0700

    Add files via upload

---
+++ Sept_28/Matt_4.8_Prob.m
@@ -0,0 +1,30 @@
+%problem 4.8
+clc;clear
+g=32.2; %ft/s^2
+v=200; %ft/s
+theta=20; %deg
+t1=0;
+h=0;
+for t=1:100
+    if h>=0
+    h(t)=v*t1*sind(theta)-0.5*g*t1^2;
+    x(t)=v*t1*cosd(theta);
+    t2(t)=t1;
+    t1=t1+0.1;
+    end
+end
+hold on
+plot(t2,h)
+title('Time(s) vs. Height(ft)')
+xlabel('Time(s)')
+ylabel('Height(ft)')
+figure
+title('Time(s) vs. Distance (ft)')
+xlabel('Time(s)')
+ylabel('Distance(ft)')
+plot(t2,x)
+figure
+title('Distance(ft) vs Height(ft)')
+xlabel('Distance(ft)')
+ylabel('Height(ft)')
+plot(x,h)
