5e4abe0d452d100f63f15fbd2557268712fcfc6d
vfmenna <145074986+vfmenna@users.noreply.github.com>
2023-09-28 17:53:45 -0700

    Add files via upload

---
+++ Sept_28/table4_2.m
@@ -0,0 +1,41 @@
+%% Table 4.2 - Examples of fprintf Commands
+
+%% Example 1
+m = 12
+d = 7532.1234
+fprintf('The value of m is %i',m)
+
+%% Example 2
+m = 12
+d = 7532.1234
+fprintf('The value of m is %i\n',m)
+
+%% Example 3
+m = 12
+d = 7532.1234
+fprintf('The value of d is %f\n',d)
+
+%% Example 4
+m = 12
+d = 7532.1234
+fprintf('The value of d is %.1f\n',d)
+
+%% Example 5
+m = 12
+d = 7532.1234
+fprintf('The value of d is %10.1f\n',d)
+
+%% Example 6
+m = 12
+d = 7532.1234
+fprintf('The value of d is %.2e\n',d)
+
+%% Example 7
+m = 12
+d = 7532.1234
+fprintf('The value of d is %.1f\n The value of m is %i\n',d,m)
+
+%% Example 8
+m = 12
+d = 7532.1234
+fprintf('The value of d is %i\n',d)
