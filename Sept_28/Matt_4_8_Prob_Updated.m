4cbb1422dcc9bf4ab86ec60ef97f93c43542ccbe
vfmenna <145074986+vfmenna@users.noreply.github.com>
2023-09-28 17:58:52 -0700

    Add files via upload

---
+++ Sept_28/Matt_4_8_Prob_Updated.m
@@ -0,0 +1,33 @@
+%problem 4.8
+clc;clear
+g=32.2; %ft/s^2
+v=200; %ft/s
+theta=20; %deg
+t1=0;
+h=0;
+for t=1:100
+    if h>=0
+    h(t)=v*t1*sind(theta)-0.5*g*t1^2;
+    x(t)=v*t1*cosd(theta);
+    t2(t)=t1;
+    t1=t1+0.1;
+    end
+end
+figure(1)
+subplot(3,1,1)
+plot(t2,h)
+title('Time(s) vs. Height(ft)')
+xlabel('Time(s)')
+ylabel('Height(ft)')
+
+subplot(3,1,2)
+plot(t2,x)
+title('Time(s) vs. Distance (ft)')
+xlabel('Time(s)')
+ylabel('Distance(ft)')
+
+subplot(3,1,3)
+plot(x,h)
+title('Distance(ft) vs Height(ft)')
+xlabel('Distance(ft)')
+ylabel('Height(ft)')
