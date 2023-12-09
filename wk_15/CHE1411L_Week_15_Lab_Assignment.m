2dee58a0db3b2f800ebb3f6474558301cdeb506f
vfmenna <145074986+vfmenna@users.noreply.github.com>
2023-12-08 23:39:41 -0800

    Add files via upload

--- wk_15/CHE1411L_Week_15_Lab_Assignment.m
+++ wk_15/CHE1411L_Week_15_Lab_Assignment.m
@@ -52,3 +52,26 @@ X = A\B
 x0 = 0.9;
 f_x = @thermo_15;
 eq_comp = fzero(f_x, x0)
+
+CO_comp_s = 1;
+H2O_comp_s = 1;
+CO2_comp_s = 0;
+H2_comp_s = 0;
+to_comp_s = 2;
+
+CO_comp_e = 1-eq_comp;
+H2O_comp_e = 1-eq_comp;
+CO2_comp_e = eq_comp;
+H2_comp_e = eq_comp;
+to_comp_e = 2;
+
+yi_co = CO_comp_e/to_comp_e;
+yi_h2o = H2O_comp_e/to_comp_e;
+yi_co2 = CO2_comp_e/to_comp_e;
+yi_h2 = H2_comp_e/to_comp_e;
+
+fprintf("The starting compositions of:\nCO was %d, \nH2O was %d, \nCO2 was %d, \nand H2 was %d.\n", CO_comp_s,H2O_comp_s,CO2_comp_s,H2_comp_s)
+fprintf("\nThe ending compositions of:\nCO was %d, \nH2O was %d, \nCO2 was %d, \nand H2 was %d.\n", CO_comp_e,H2O_comp_e,CO2_comp_e,H2_comp_e)
+fprintf("\nThe yi of:\nCO was %d, \nH2O was %d, \nCO2 was %d, \nand H2 was %d.\n", yi_co, yi_h2o, yi_co2, yi_h2)
+
+
---
+++ wk_15/thermo_15.m
@@ -0,0 +1,3 @@
+function f = thermo_15(x)
+
+f = 148.4-(x^2/(1-x)^2);
