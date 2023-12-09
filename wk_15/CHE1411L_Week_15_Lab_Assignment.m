5d9bbf9f8072757cee5e4af07ce12c827070fd13
vfmenna <145074986+vfmenna@users.noreply.github.com>
2023-12-08 22:41:56 -0800

    Add files via upload

---
+++ wk_15/CHE1411L_Week_15_Lab_Assignment.m
@@ -0,0 +1,54 @@
+%% CHE 1411L Assignment Week 15
+%% Problem 1a: Solve the following set of equations using MATLAB to determine no, unique, or an infinite solution.
+
+A = [-6 2 -2;-3 4 -3;2 4 -7];
+B = [15;13;-9];
+
+ra_a = rank(A)
+ra_b = rank(B)
+ra_c = ra_a/ra_b
+
+if ra_a == ra_b || ra_a == length(A)
+   fprintf("The set as a unique solution.")
+else if ra_a == ra_b || ra_b < length(A)
+    fprintf("The set has infinite solutions.")
+else
+    fprintf("The set has no solution.")
+end
+end
+
+X = inv(A) * B
+
+%% Problem 1b: Solve the following set of equations using MATLAB to determine no, unique, or an infinite solution.
+
+A = [1 1;1 -1;2 -5];
+B = [3;1;10];
+
+ra_a = rank(A)
+ra_b = rank(B)
+ra_c = ra_a/ra_b
+
+if ra_a == ra_b || ra_a == length(A)
+   fprintf("The set as a unique solution.")
+else if ra_a == ra_b || ra_b < length(A)
+    fprintf("The set has infinite solutions. Use left division.")
+else
+    fprintf("The set has no solution.")
+end
+end
+
+X = A\B
+
+%% Problem 2: Solve the system of two nonlinear equations using Excel solver.
+% This problem was solved in Excel attached at the end of this MATLAB
+% published pdf.
+
+%% Problem 3a: Use Goal Seek and Solver in Excel to solve the problem.
+% This problem was solved in Excel attached at the end of this MATLAB
+% published pdf.
+
+%% Problem 3b: Use MATLAB to solve the problem: a) create a function, b) create a script using 'fzero' and format output at 'fprintf'
+
+x0 = 0.9;
+f_x = @thermo_15;
+eq_comp = fzero(f_x, x0)
